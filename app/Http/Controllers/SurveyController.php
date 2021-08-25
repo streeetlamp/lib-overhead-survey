c176b3a559074ae4d5e7cc57c9713bddcc8ae90a
cody <hi@codywhitby.net>
2021-08-25 13:05:19 -0400

    cmon

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -21,16 +21,6 @@ class SurveyController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('surveys.create');
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
