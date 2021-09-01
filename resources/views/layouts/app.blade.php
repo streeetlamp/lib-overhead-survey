0f9f42a98c8c16b5aa4c8b21473cb8280c362b7b
cody <hi@codywhitby.net>
2021-09-01 10:38:53 -0400

    vcu bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     </script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous">
     </script>
+    <script type="text/javascript" src="//branding.vcu.edu/bar/academic/latest.js"></script>
 
 
 </head>
