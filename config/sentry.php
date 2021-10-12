94e3cca34488962da2a321021cb1f55d7c6e9ad8
cody <hi@codywhitby.net>
2021-10-12 09:48:07 -0400

    sentry update

--- config/sentry.php
+++ config/sentry.php
@@ -5,10 +5,10 @@ return [
     'dsn' => env('SENTRY_LARAVEL_DSN', env('SENTRY_DSN')),
 
     // capture release as git sha
-    // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
+    'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
 
     // When left empty or `null` the Laravel environment will be used
-    'environment' => env('SENTRY_ENVIRONMENT'),
+    'environment' => env(''),
 
     'breadcrumbs' => [
         // Capture Laravel logs in breadcrumbs
@@ -29,7 +29,7 @@ return [
 
     'tracing' => [
         // Trace queue jobs as their own transactions
-        'queue_job_transactions' => env('SENTRY_TRACE_QUEUE_ENABLED', false),
+        'queue_job_transactions' => env('SENTRY_TRACE_QUEUE_ENABLED', true),
 
         // Capture queue jobs as spans when executed on the sync driver
         'queue_jobs' => true,
@@ -48,9 +48,9 @@ return [
     ],
 
     // @see: https://docs.sentry.io/platforms/php/configuration/options/#send-default-pii
-    'send_default_pii' => false,
+    'send_default_pii' => true,
 
-    'traces_sample_rate' => (float)(env('SENTRY_TRACES_SAMPLE_RATE', 0.0)),
+    'traces_sample_rate' => (float)(env('SENTRY_TRACES_SAMPLE_RATE', 1)),
 
     'controllers_base_namespace' => env('SENTRY_CONTROLLERS_BASE_NAMESPACE', 'App\\Http\\Controllers'),
 
